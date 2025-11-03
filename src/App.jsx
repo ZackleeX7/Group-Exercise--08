32afc398788173d07bcd2d210677e32ff7fa25da
ZackleeX7 <S22310026@student.unklab.ac.id>
2025-11-03 19:04:18 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  let hello = "Hello World";
+  return (
+  <>
+    <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif">
+      {hello}
+    </div>
+  </>
+  );
+};
+
+export default App;
