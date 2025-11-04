396c824428de9f30323e7f39af66339149ae18c8
ZackleeX7 <S22310026@student.unklab.ac.id>
2025-11-04 19:56:39 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,6 @@ import Gen1 from "./Gen1";
 import Gen2 from "./Gen2";
 import Gen3 from "./Gen3";
 import Gen4 from "./Gen4";
-
-const App = () => {
-  return (
-  <>
-    <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif">
-      
-    </div>
-    <Gen1/>
-    <Gen2/>
-    <Gen3/>
-    <Gen4/>
-  </>
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
@@ -22,7 +10,21 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
-      <Home />
+
+      {/* Bagian konten utama */}
+      <main className="flex-1">
+        <div className="text-[25px] bg-blue-500 m-10 p-10 font-serif">
+          {/* Kamu bisa isi teks atau elemen lain di sini */}
+        </div>
+
+        <Gen1 />
+        <Gen2 />
+        <Gen3 />
+        <Gen4 />
+
+        <Home />
+      </main>
+
       <Footer />
     </div>
   );
