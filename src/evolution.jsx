6a1e0fbe521392ccc8e2ca65510a96bd7e5f4bab
ZackleeX7 <s22310026@student.unklab.ac.id>
2025-11-04 20:10:40 +0800

    Update evolution.jsx

--- src/evolution.jsx
+++ src/evolution.jsx
@@ -1,1 +1,11 @@
+function evolution({ text }) {
+    return (
+        <div className="bg-white rounded-lg shadow-md p-4 m-4 w-60 text-center">
+        <h1 className="text-3xl font-bold text-black-500 drop-shadow-md">
+            Ini adalah komponen evolution
+        </h1>
+        </div>
+    );
+}
 
+export default evolution;
