8fb25ff8dbf697543e2c61ebd2e2436ffd884170
ZackleeX7 <s22310026@student.unklab.ac.id>
2025-11-04 20:11:47 +0800

    Update logo2.jsx

--- src/logo2.jsx
+++ src/logo2.jsx
@@ -1,1 +1,11 @@
+function logo2({ text }) {
+    return (
+        <div className="bg-white rounded-lg shadow-md p-4 m-4 w-60 text-center">
+        <h1 className="text-3xl font-bold text-black-500 drop-shadow-md">
+            Ini adalah logo card info
+        </h1>
+        </div>
+    );
+}
 
+export default logo2;
