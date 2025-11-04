fc5b2f6800375a83305093b7bb13cf2036199b4f
SolideaRumopa <rumopasolidea@gmail.com>
2025-11-04 11:17:18 +0800

    Restore Footer.jsx content

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center mt-8">
+      &copy; 2025 Kelompok 8 - All rights reserved
+    </footer>
+  );
+}
+
+export default Footer;
